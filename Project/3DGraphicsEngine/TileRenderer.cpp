a94a3f4eee3671d5b727b2ab8e7cbb21086526c2
Wildman0 <kylewildman98@gmail.com>
2019-01-31 20:50:01 +0000

    Update TileRenderer.cpp

--- Project/3DGraphicsEngine/TileRenderer.cpp
+++ Project/3DGraphicsEngine/TileRenderer.cpp
@@ -9,14 +9,15 @@ TileRenderer::TileRenderer()
 //Renders all tiles in a multidimensional tile vector
 void TileRenderer::renderTileMap(std::vector<std::vector<Tile>> tileMap, sf::RenderWindow* window)
 {
+	sf::CircleShape cS = sf::CircleShape(16);
+	cS.setFillColor(sf::Color::Green);
+
 	for (size_t i = 0; i < tileMap.capacity(); i++)
 	{
 		for (size_t j = 0; j < tileMap[0].capacity(); j++)
 		{
-			sf::CircleShape cS = sf::CircleShape(16);
 			cS.setPosition(i * 32, j * 32);
-			cS.setFillColor(sf::Color::Green);
-
+			
 			window->draw(cS);
 		}
 	}
